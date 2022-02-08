e0fcbf64e5ed5ec379d6218347b0f784d73520c3
rajaa1272 <arepellinagaraj1272@gmail.com>
2022-02-08 12:54:46 +0530

    new repo

--- pubrepo.py
+++ pubrepo.py
@@ -1,8 +1,8 @@
 
-a = input(" a : ")
-b = input (" b : ")
+x = input(" type a number : ")
+y = input (" type another number  : ")
 
 
-sum = int (a) + int (b)
+sum = int (x) + int (y)
 
 print("this is the sum", sum)
